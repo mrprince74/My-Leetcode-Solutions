0ba95d32dd5cf0c717f189bc6cb1dcfb5f4a0b9e
mrprince74 <160225164+mrprince74@users.noreply.github.com>
2025-07-06 15:12:49 +0300

    Time: 53 ms (92.28%), Space: 81.9 MB (70.15%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -1,23 +1,25 @@
 class FindSumPairs {
-    map<int,int> freq;
+    unordered_map<int,int> freq1, freq2;
     vector<int> nums1, nums2;
 public:
     FindSumPairs(vector<int>& nums1, vector<int>& nums2): nums1(nums1), nums2(nums2) {
+        for(int num : nums1)
+            freq1[num]++;
         for(int num : nums2)
-            freq[num]++;
+            freq2[num]++;
     }
     
     void add(int index, int val) {
-        freq[nums2[index]]--;
+        freq2[nums2[index]]--;
         nums2[index]+= val;
-        freq[nums2[index]]++;
+        freq2[nums2[index]]++;
     }
     
     int count(int tot) {
         int ans = 0;
-        for(int num : nums1){
-            if (freq.count(tot - num))
-                ans+= freq[tot - num];
+        for(auto [num,f] : freq1){
+            if (freq2.count(tot - num))
+                ans+= f * freq2[tot - num];
         }
         return ans;
     }
