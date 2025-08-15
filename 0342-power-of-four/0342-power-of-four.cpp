ec684ceb9bf2201b3f49eb13a48c1344749f5e32
mrprince74 <160225164+mrprince74@users.noreply.github.com>
2025-08-15 11:46:14 +0300

    Time: 0 ms (100%), Space: 7.8 MB (82.36%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,7 +3,6 @@ public:
     bool isPowerOfFour(int n) {
         if ((n <= 0) || (n & (n-1)))
             return false;
-        cout << n << "\n";
         int pos = 31 - __builtin_clz(n);
         return pos & 1 ^ 1;
     }
